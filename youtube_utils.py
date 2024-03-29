1341ddc134dfb6078a9fdfd78873c2e02b4f57d4
mohamedhassan218 <m.hassan.def@gmail.com>
2024-03-29 21:52:57 +0200

    Resolve the youtube_handler() and add comments

--- youtube_utils.py
+++ youtube_utils.py
@@ -1,36 +1,32 @@
-import streamlit as st
+"""
+    Module contains functions that handle the work with youtube URLs.
+    
+    @author  Mohamed Hassan
+    @version 1.0
+    @since   2024-3-29
+"""
+
 from langchain_community.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
-def youtube_handler(url: str):
-    # st.write("youtube_utils entered")
-    # st.title("url")
-    # st.write(url)
+def youtube_handler(url):
+    """
+    This function is used to grap the content of the Youtube video by catch the 
+    transcript of the video then split it into chunks.
+
+    @param url: a string representing the URL of the video.
+    @return result: list of strings represents the chunks of the video transcript.
+    """
     loader = YoutubeLoader.from_youtube_url(url)
     transcript = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     chunks = text_splitter.split_documents(transcript)
-    # loader = YoutubeLoader.from_youtube_url(url)
-    # transcript = loader.load()
-    # st.title("Transcript")
-    # st.write(transcript)
-    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=250)
-    # chunks = text_splitter.split_documents(transcript)
-    # st.title("Chunks")
-    # st.write(chunks)
-    # st.write("youtube_utils ended")
-    # for c in chunks:
-    #     print(c)
-    # print("\n")
     result = [doc.page_content for doc in chunks]
     return result
 
 
-# chunks = youtube_handler('https://youtu.be/EzTxYQmU8OE?si=EXIZ-dm7QpuCvBdF')
-
-# for c in chunks:
-#     print(c.page_content)
-#     print('\n')
-
-# # print(chunks)
+if __name__ == "__main__":
+    chunks = youtube_handler("https://youtu.be/EzTxYQmU8OE?si=EXIZ-dm7QpuCvBdF")
+    print(f"Length of chunks: {len(chunks)}")
+    print(chunks)
